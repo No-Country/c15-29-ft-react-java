2cec3e26b0a9148450258c2985d0e41093fec212
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-12-01 01:20:25 -0300

    commit

--- Client/src/app/register/page.jsx
+++ Client/src/app/register/page.jsx
@@ -2,13 +2,36 @@ import React from "react";
 
 export default function RegisterPage() {
   return (
-    <div>
-      <form action="">
-        <input type="text" />
-        <input type="email" />
-        <input type="password" />
-        <input type="password" />
-        <input type="submmit"> </input>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h3 className="text-2xl font-bold mb-4">REGISTER</h3>
+      <form className="flex flex-col">
+        <input
+          className="mb-2 p-2 border border-gray-300 rounded"
+          type="text"
+          placeholder="USERNAME"
+        />
+        <input
+          className="mb-2 p-2 border border-gray-300 rounded"
+          type="email"
+          placeholder="E-MAIL"
+        />
+        <input
+          className="mb-2 p-2 border border-gray-300 rounded"
+          type="password"
+          placeholder="PASSWORD"
+        />
+        <input
+          className="mb-2 p-2 border border-gray-300 rounded"
+          type="password"
+          placeholder="CONFIRM PASSWORD"
+        />
+        <h5 className="text-sm mb-2">REQUIRED FIELD</h5>
+        <button
+          className="bg-blue-500 text-white py-2 px-4 rounded"
+          type="submit"
+        >
+          REGISTER
+        </button>
       </form>
     </div>
   );
