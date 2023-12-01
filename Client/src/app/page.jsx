8b06f156a870ba3da6b7e70dc8712671cbccef82
Adel Fetner <adelfetner@gmail.com>
2023-12-01 08:46:10 -0300

    Init Card component, todo. Added it to app

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/Navbar'
+import Card from '@/components/Card'
 import React from 'react'
 
 export default function HomePage() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div>HomePage</div>
+      <Card />
     </>
   )
 }
---
+++ Client/src/components/Card.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Card = () => {
+    return (
+        <article className="flex flex-col rounded-xl shadow-lg overflow-hidden">
+
+        </article>
+    )
+}
+
+export default Card;
