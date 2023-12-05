e01c17931677a27d73f3077f7ac0062e16b19a32
Adel Fetner <adelfetner@gmail.com>
2023-12-04 22:18:20 -0300

    Fixed navbar bug, was missing a path on tailwind.config.js.

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,10 +1,11 @@
 // tailwind.config.js
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     // ...
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
