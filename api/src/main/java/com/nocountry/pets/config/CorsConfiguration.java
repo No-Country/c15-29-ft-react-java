4d2e7a57cc063a78de82b2889a96aa08be780879
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-12-06 12:26:12 -0400

    Fix to allowed origins

--- api/src/main/java/com/nocountry/pets/config/CorsConfiguration.java
+++ api/src/main/java/com/nocountry/pets/config/CorsConfiguration.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/")
-                .allowedOrigins("http://localhost:3000/")
-                .allowedOrigins("/**")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
