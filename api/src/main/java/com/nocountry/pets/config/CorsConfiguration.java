25bdb74cce1064e6a80d992563c38ee3db65ff09
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-12-06 12:16:44 -0400

    fix the cors for login

---
+++ api/src/main/java/com/nocountry/pets/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.nocountry.pets.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedOrigins("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
