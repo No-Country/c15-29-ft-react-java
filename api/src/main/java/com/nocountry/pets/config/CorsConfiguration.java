2aea440cb6373f95ca94d92e3b140d4233cfd85e
Juan Ortega <91577323+Full-Juan-Ortega@users.noreply.github.com>
2023-12-07 17:29:22 -0300

    cors config closed

--- api/src/main/java/com/nocountry/pets/config/CorsConfiguration.java
+++ api/src/main/java/com/nocountry/pets/config/CorsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
-/*    @Override
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
@@ -14,13 +14,13 @@ public class CorsConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
-    }*/
+    }
     //CORS TOTALLY OPEN
-    @Override
+/*    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Permitir cualquier ruta
                 .allowedOrigins("*")  // Permitir cualquier origen
                 .allowedMethods("*")  // Permitir cualquier método HTTP
                 .allowCredentials(false);  // No permitir credenciales
-    }
+    }*/
 }
