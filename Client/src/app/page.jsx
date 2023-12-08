9008e25cd7335622882a8711218579e3ec68c8d1
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-12-07 23:48:26 -0300

    commit

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Carousel from '@/components/carousel/page'
 import React from 'react'
 
 export default function HomePage() {
   return (
     <>
+     <Carousel/>
       {/* <div className='mt-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nam tempore non? Ipsam corrupti possimus eaque aperiam mollitia in, ea eius accusamus aliquid! Magni repudiandae asperiores voluptates nihil, dicta autem.</div> */}
     </>
   )
