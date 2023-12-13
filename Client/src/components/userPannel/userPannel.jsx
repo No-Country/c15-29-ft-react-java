155314e874fbfd3e2ca71fd03a238ce512194823
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-12-13 11:07:23 -0300

    commit

--- Client/src/components/userPannel/userPannel.jsx
+++ Client/src/components/userPannel/userPannel.jsx
@@ -1,28 +1,24 @@
 import {
   Button,
-  Checkbox,
   Input,
-  Link,
   Modal,
   ModalBody,
   ModalContent,
+  Avatar,
+  AvatarIcon,
   ModalFooter,
   ModalHeader,
   useDisclosure,
 } from "@nextui-org/react";
 
 import React, { useState } from "react";
-import { MailIcon } from "@/components/login/Mailicon";
-import { LockIcon } from "@/components/login/LockIcon";
+
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 export default function RegisterModal() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
   const [credentials, setCredentials] = useState({
     name: "",
-    lastName: "",
     email: "",
     password: "",
   });
@@ -52,16 +48,37 @@ export default function RegisterModal() {
 
   return (
     <>
-      <Link onPress={onOpen}>Login</Link>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-        <ModalContent>
-          {(onClose) => (
+      <form onSubmit={handleSubmit}>
+        <div className="flex items-center">
+          <Avatar
+            icon={<AvatarIcon />}
+            classNames={{
+              base: "bg-gradient-to-br from-[#FFB457] to-[#FF705B]",
+              icon: "text-black/80",
+            }}
+          />
+        </div>
+        <Modal placement="top-center">
+          <ModalContent>
             <>
               <form onSubmit={handleSubmit}>
                 <ModalHeader className="flex flex-col gap-1">
-                  Sign Up
+                  Fill all the Fields
                 </ModalHeader>
                 <ModalBody>
+                  <Input
+                    type="text"
+                    onChange={(e) =>
+                      setCredentials({
+                        ...credentials,
+                        username: e.target.value,
+                      })
+                    }
+                    autoFocus
+                    label="Username"
+                    placeholder="Enter your username"
+                    variant="bordered"
+                  />
                   <Input
                     type="text"
                     onChange={(e) =>
@@ -71,27 +88,8 @@ export default function RegisterModal() {
                       })
                     }
                     autoFocus
-                    endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }
                     label="name"
                     placeholder="Enter your Name"
-                    variant="bordered"
-                  />
-                  <Input
-                    type="text"
-                    onChange={(e) =>
-                      setCredentials({
-                        ...credentials,
-                        lastName: e.target.value,
-                      })
-                    }
-                    autoFocus
-                    endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }
-                    label="name"
-                    placeholder="Enter your Lastname"
                     variant="bordered"
                   />
                   <Input
@@ -103,27 +101,36 @@ export default function RegisterModal() {
                       })
                     }
                     autoFocus
-                    endContent={
-                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                    }
-                    label="email"
+                    label="Email"
                     placeholder="Enter your Email"
                     variant="bordered"
                   />
 
                   <Input
+                    type="text"
                     onChange={(e) =>
                       setCredentials({
                         ...credentials,
-                        password: e.target.value,
+                        username: e.target.value,
                       })
                     }
-                    endContent={
-                      <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    autoFocus
+                    label="Username"
+                    placeholder="Enter your username"
+                    variant="bordered"
+                  />
+
+                  <Input
+                    type="text"
+                    onChange={(e) =>
+                      setCredentials({
+                        ...credentials,
+                        username: e.target.value,
+                      })
                     }
-                    label="password"
-                    placeholder="Enter your Password"
-                    type="password"
+                    autoFocus
+                    label="Username"
+                    placeholder="Enter your username"
                     variant="bordered"
                   />
                 </ModalBody>
@@ -132,14 +139,14 @@ export default function RegisterModal() {
                     Close
                   </Button>
                   <Button type="submit" color="primary">
-                    Sign up
+                    Ready
                   </Button>
                 </ModalFooter>
               </form>
             </>
-          )}
-        </ModalContent>
-      </Modal>
+          </ModalContent>
+        </Modal>
+      </form>
     </>
   );
 }
