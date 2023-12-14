73f2e4e60dba4151884af863b82093133f328be6
Adel Fetner <adelfetner@gmail.com>
2023-12-13 21:36:31 -0300

    Init branch. Created new folder and files.

---
+++ Client/src/app/createPet/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from "react"
+import createPet from "@/components/createPet/CreatePet"
+
+export default function CreatePetPage() {
+    return (
+        <>
+            <div>CreatePetPage</div>
+            <createPet />
+        </>
+    )
+}
