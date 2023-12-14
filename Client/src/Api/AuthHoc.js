7f5901d636711613f477e17a09baa87cba2021a2
Sebastian Ocando <ocandovivas@gmail.com>
2023-12-14 13:02:16 -0300

    spinner loading

--- Client/src/Api/AuthHoc.js
+++ Client/src/Api/AuthHoc.js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuth } from "@/Api/AuthContext";
+import { LoadingSpinner } from "@/components/LoadingSpinner";
 
 export const AuthHoc = (WrappedComponent) => {
   const AuthHoc = (props) => {
@@ -8,31 +9,37 @@ export const AuthHoc = (WrappedComponent) => {
     const router = useRouter();
 
     useEffect(() => {
-      // Si el token no está presente, redirige al login
-      if (!token && !loading) {
-        router.push('/');
-      }
+      setTimeout(() => {
+
+        // Si el token no está presente y no está cargando, redirige al login
+
+        if (!token && !loading) {
+          router.push("/");
+        }
+      }, 800);
+
     }, [token, loading, router]);
 
-    // Si el token no está presente, podrías mostrar un mensaje de carga o redirigir al login
+    // Si el token no está presente y no está cargando, muestra un mensaje de carga
+
     if (!token || loading) {
-      return <p>Cargando...</p>;
+      return <LoadingSpinner />;
     }
 
     // Renderiza el componente envuelto si el token está presente
+
     return <WrappedComponent {...props} />;
   };
 
   // Asigna un nombre al componente para evitar el warning
+
   AuthHoc.displayName = `AuthHoc(${getDisplayName(WrappedComponent)})`;
 
   return AuthHoc;
 };
 
 // Función para obtener el nombre de un componente envuelto
+
 function getDisplayName(WrappedComponent) {
-  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
 }
-
-
-
