604ce4c91e943b877d320632fd0b41afd4a0d1e6
enrique moreira <77258003+enrique434@users.noreply.github.com>
2023-12-16 12:57:04 -0500

    joining footer on the about

--- Client/src/app/page.jsx
+++ Client/src/app/page.jsx
@@ -3,19 +3,14 @@
 import Carousel from '@/components/carousel/page'
 import React from 'react'
 import LandingPage from '@/components/landingPage/landingPage'
+import Footer from '@/components/footer/Footer'
 
 export default function HomePage() {
   return (
     <>
-
-
-
+   
      <LandingPage />
-
-
-
-
-
+    <Footer />
 
 
     </>
