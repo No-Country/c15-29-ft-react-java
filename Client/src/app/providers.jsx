9d500763c2bfa4fb84c980298b54e990685a91f7
Sebastian Ocando <ocandovivas@gmail.com>
2023-12-17 15:28:30 -0300

    Not found fixed

--- Client/src/app/providers.jsx
+++ Client/src/app/providers.jsx
@@ -3,15 +3,13 @@
 import { NextUIProvider } from "@nextui-org/react";
 import Toast from "@/components/toast/Toast";
 import { AuthProvider } from "@/Api/AuthContext";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import { NavbarCheck } from "@/components/navbar/NavbarCheck";
 
 export function Providers({ children }) {
   return (
     <NextUIProvider>
       <AuthProvider>
-        {/* <NavbarCheck /> */}
+        <NavbarCheck />
         <Toast />
         {children}
       </AuthProvider>
