1f7067d07bd3439679257659d5f322d8330db932
Adel Fetner <adelfetner@gmail.com>
2023-12-22 15:58:47 -0300

    More changes for feature carrousel

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -9,7 +9,20 @@ const config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'raining': 'raining 2s linear infinite',
+      }
+    },
+    keyframes: {
+      raining: {
+        '0%': { ransform: 'translateY(-20vh)', },
+        "25%": { transform: "translateY(0vh)" },
+        // "50%": { transform: "translateY(100vh)" },
+        "75%": { transform: "translateY(0vh)" },
+        "100%": { transform: "translateY(20vh)" }
+      }
+    }
   },
   darkMode: "class",
   plugins: [nextui()]
